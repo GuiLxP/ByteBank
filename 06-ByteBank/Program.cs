5080819fe9d0703e510c0085bb7904e57298b0e8
Guilherme Pantoja <guilhermematheus166@gmail.com>
2022-05-17 23:15:55 -0300

    Add files via upload

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
+
+            cliente.Nome = "Guilherme";
+            cliente.Cpf = "102.366.879-30";
+            cliente.Profissao = "Desenvolvedor";
+
+
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+
+            Console.WriteLine(conta.Titular.Nome);
+            Console.WriteLine(conta.Saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
